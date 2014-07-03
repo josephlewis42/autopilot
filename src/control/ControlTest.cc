98881c9e59477d4bc4b16d6b4e4575aa4a493ed6
Joseph <joehms22@gmail.com>
2014-07-03 09:35:40 -0600

    Added test of expected failure in ControlTest and extra expects to catch subtle changes that could be happening not as a result of the tests.

--- src/control/ControlTest.cc
+++ src/control/ControlTest.cc
@@ -15,7 +15,7 @@ TEST(Control, SET_PARAM_ROLL_KP)
 {
     float first = Control::getInstance()->attitude_pid_controller().get_roll_proportional();
     Parameter p(attitude_pid::PARAM_ROLL_KP, first * .5, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->attitude_pid_controller().get_roll_proportional();
 
     EXPECT_NE(first, second);
@@ -25,7 +25,7 @@ TEST(Control, SET_PARAM_ROLL_KD)
 {
     float first = Control::getInstance()->attitude_pid_controller().get_roll_derivative();
     Parameter p(attitude_pid::PARAM_ROLL_KD, first * .5, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->attitude_pid_controller().get_roll_derivative();
 
     EXPECT_NE(first, second);
@@ -35,7 +35,7 @@ TEST(Control, SET_PARAM_ROLL_KI)
 {
     float first = Control::getInstance()->attitude_pid_controller().get_roll_integral();
     Parameter p(attitude_pid::PARAM_ROLL_KI, first * .5, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->attitude_pid_controller().get_roll_integral();
 
     EXPECT_NE(first, second);
@@ -46,7 +46,7 @@ TEST(Control, SET_PARAM_PITCH_KP)
 {
     float first = Control::getInstance()->attitude_pid_controller().get_pitch_proportional();
     Parameter p(attitude_pid::PARAM_PITCH_KP, first + 1, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->attitude_pid_controller().get_pitch_proportional();
 
     EXPECT_NE(first, second);
@@ -56,7 +56,7 @@ TEST(Control, SET_PARAM_PITCH_KD)
 {
     float first = Control::getInstance()->attitude_pid_controller().get_pitch_derivative();
     Parameter p(attitude_pid::PARAM_PITCH_KD, first + 1, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->attitude_pid_controller().get_pitch_derivative();
 
     EXPECT_NE(first, second);
@@ -66,7 +66,7 @@ TEST(Control, SET_PARAM_PITCH_KI)
 {
     float first = Control::getInstance()->attitude_pid_controller().get_pitch_integral();
     Parameter p(attitude_pid::PARAM_PITCH_KI, first + 1, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->attitude_pid_controller().get_pitch_integral();
 
     EXPECT_NE(first, second);
@@ -76,7 +76,7 @@ TEST(Control, SET_PARAM_ROLL_TRIM)
 {
     float first = Control::getInstance()->attitude_pid_controller().get_roll_trim_degrees();
     Parameter p(attitude_pid::PARAM_ROLL_TRIM, first + 1, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->attitude_pid_controller().get_roll_trim_degrees();
 
     EXPECT_NE(first, second);
@@ -87,7 +87,7 @@ TEST(Control, SET_PARAM_PITCH_TRIM)
 {
     float first = Control::getInstance()->attitude_pid_controller().get_pitch_trim_degrees();
     Parameter p(attitude_pid::PARAM_PITCH_TRIM, first + 1, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->attitude_pid_controller().get_pitch_trim_degrees();
 
     EXPECT_NE(first, second);
@@ -97,7 +97,7 @@ TEST(Control, SET_PARAM_X_KP)
 {
     float first = Control::getInstance()->translation_pid_controller().get_x_proportional();
     Parameter p(translation_outer_pid::PARAM_X_KP, first + 1, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->translation_pid_controller().get_x_proportional();
 
     EXPECT_NE(first, second);
@@ -107,7 +107,7 @@ TEST(Control, SET_PARAM_X_KD)
 {
     float first = Control::getInstance()->translation_pid_controller().get_x_derivative();
     Parameter p(translation_outer_pid::PARAM_X_KD, first + 1, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->translation_pid_controller().get_x_derivative();
 
     EXPECT_NE(first, second);
@@ -117,7 +117,7 @@ TEST(Control, SET_PARAM_X_KI)
 {
     float first = Control::getInstance()->translation_pid_controller().get_x_integral();
     Parameter p(translation_outer_pid::PARAM_X_KI, first + 1, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->translation_pid_controller().get_x_integral();
 
     EXPECT_NE(first, second);
@@ -127,7 +127,7 @@ TEST(Control, SET_PARAM_Y_KP)
 {
     float first = Control::getInstance()->translation_pid_controller().get_y_proportional();
     Parameter p(translation_outer_pid::PARAM_Y_KP, first + 1, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->translation_pid_controller().get_y_proportional();
 
     EXPECT_NE(first, second);
@@ -137,7 +137,7 @@ TEST(Control, SET_PARAM_Y_KD)
 {
     float first = Control::getInstance()->translation_pid_controller().get_y_derivative();
     Parameter p(translation_outer_pid::PARAM_Y_KD, first + 1, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->translation_pid_controller().get_y_derivative();
 
     EXPECT_NE(first, second);
@@ -147,7 +147,7 @@ TEST(Control, SET_PARAM_Y_KI)
 {
     float first = Control::getInstance()->translation_pid_controller().get_y_integral();
     Parameter p(translation_outer_pid::PARAM_Y_KI, first + 1, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->translation_pid_controller().get_y_integral();
 
     EXPECT_NE(first, second);
@@ -157,7 +157,7 @@ TEST(Control, SET_PARAM_TRAVEL)
 {
     float first = Control::getInstance()->translation_pid_controller().scaled_travel_degrees();
     Parameter p(translation_outer_pid::PARAM_TRAVEL, first + 1, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->translation_pid_controller().scaled_travel_degrees();
 
     EXPECT_NE(first, second);
@@ -167,7 +167,7 @@ TEST(Control, SET_PARAM_TRAVEL_SBF)
 {
     float first = Control::getInstance()->x_y_sbf_controller.scaled_travel_degrees();
     Parameter p(tail_sbf::PARAM_TRAVEL, first + 1, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->x_y_sbf_controller.scaled_travel_degrees();
 
     EXPECT_NE(first, second);
@@ -177,7 +177,7 @@ TEST(Control, SET_PARAM_X_KP_TAIL)
 {
     float first = Control::getInstance()->x_y_sbf_controller.get_x_proportional();
     Parameter p(tail_sbf::PARAM_X_KP, first + 1, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->x_y_sbf_controller.get_x_proportional();
 
     EXPECT_NE(first, second);
@@ -187,7 +187,7 @@ TEST(Control, SET_PARAM_X_KD_TAIL)
 {
     float first = Control::getInstance()->x_y_sbf_controller.get_x_derivative();
     Parameter p(tail_sbf::PARAM_X_KD, first + 1, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->x_y_sbf_controller.get_x_derivative();
 
     EXPECT_NE(first, second);
@@ -197,7 +197,7 @@ TEST(Control, SET_PARAM_X_KI_TAIL)
 {
     float first = Control::getInstance()->x_y_sbf_controller.get_x_integral();
     Parameter p(tail_sbf::PARAM_X_KI, first + 1, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->x_y_sbf_controller.get_x_integral();
 
     EXPECT_NE(first, second);
@@ -207,7 +207,7 @@ TEST(Control, SET_PARAM_Y_KP_TAIL)
 {
     float first = Control::getInstance()->x_y_sbf_controller.get_y_proportional();
     Parameter p(tail_sbf::PARAM_Y_KP, first + 1, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->x_y_sbf_controller.get_y_proportional();
 
     EXPECT_NE(first, second);
@@ -217,7 +217,7 @@ TEST(Control, SET_PARAM_Y_KD_TAIL)
 {
     float first = Control::getInstance()->x_y_sbf_controller.get_y_derivative();
     Parameter p(tail_sbf::PARAM_Y_KD, first + 1, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->x_y_sbf_controller.get_y_derivative();
 
     EXPECT_NE(first, second);
@@ -227,7 +227,7 @@ TEST(Control, SET_PARAM_Y_KI_TAIL)
 {
     float first = Control::getInstance()->x_y_sbf_controller.get_y_integral();
     Parameter p(tail_sbf::PARAM_Y_KI, first + 1, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->x_y_sbf_controller.get_y_integral();
 
     EXPECT_NE(first, second);
@@ -237,7 +237,7 @@ TEST(Control, SET_PARAM_HOVER_TIME_CIRCLE)
 {
     float first = Control::getInstance()->circle_trajectory.get_hover_time();
     Parameter p(circle::PARAM_HOVER_TIME, first + 1, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->circle_trajectory.get_hover_time();
 
     EXPECT_NE(first, second);
@@ -247,7 +247,7 @@ TEST(Control, SET_PARAM_RADIUS)
 {
     float first = Control::getInstance()->circle_trajectory.get_radius();
     Parameter p(circle::PARAM_RADIUS, first + 1, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->circle_trajectory.get_radius();
 
     EXPECT_NE(first, second);
@@ -257,7 +257,7 @@ TEST(Control, SET_PARAM_SPEED_CIRCLE)
 {
     float first = Control::getInstance()->circle_trajectory.get_speed();
     Parameter p(circle::PARAM_SPEED, first + 1, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->circle_trajectory.get_speed();
 
     EXPECT_NE(first, second);
@@ -267,7 +267,7 @@ TEST(Control, SET_PARAM_HOVER_TIME_LINE)
 {
     float first = Control::getInstance()->line_trajectory.get_hover_time();
     Parameter p(line::PARAM_HOVER_TIME, first + 1, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->line_trajectory.get_hover_time();
 
     EXPECT_NE(first, second);
@@ -277,7 +277,7 @@ TEST(Control, SET_PARAM_SPEED_LINE)
 {
     float first = Control::getInstance()->line_trajectory.get_speed();
     Parameter p(line::PARAM_SPEED, first + 1, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->line_trajectory.get_speed();
 
     EXPECT_NE(first, second);
@@ -287,7 +287,7 @@ TEST(Control, SET_PARAM_X_TRAVEL)
 {
     float first = Control::getInstance()->line_trajectory.get_x_travel();
     Parameter p(line::PARAM_X_TRAVEL, first + 1, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->line_trajectory.get_x_travel();
 
     EXPECT_NE(first, second);
@@ -297,7 +297,7 @@ TEST(Control, SET_PARAM_Y_TRAVEL)
 {
     float first = Control::getInstance()->line_trajectory.get_y_travel();
     Parameter p(line::PARAM_Y_TRAVEL, first + 1, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->line_trajectory.get_y_travel();
 
     EXPECT_NE(first, second);
@@ -307,7 +307,7 @@ TEST(Control, SET_PARAM_MIX_ROLL)
 {
     float first = Control::getInstance()->get_roll_mix();
     Parameter p(Control::PARAM_MIX_ROLL, first * .5, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->get_roll_mix();
 
     EXPECT_NE(first, second);
@@ -317,8 +317,16 @@ TEST(Control, SET_PARAM_MIX_PITCH)
 {
     float first = Control::getInstance()->get_pitch_mix();
     Parameter p(Control::PARAM_MIX_PITCH, first * .5, 0);
-    Control::getInstance()->setParameter(p);
+    EXPECT_TRUE(Control::getInstance()->setParameter(p));
     float second = Control::getInstance()->get_pitch_mix();
 
     EXPECT_NE(first, second);
+}
+
+
+TEST(Control, EMPTY_PARAM_DOES_NOT_EXIST)
+{
+    // we should not allow an empty param.
+    Parameter p("", 1, 0);
+    EXPECT_FALSE(Control::getInstance()->setParameter(p));
 }
